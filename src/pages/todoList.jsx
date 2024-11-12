460b838079675bd6bc5ac0d28739dedd882f8d92
Chintan-Pidhadiya <ultracubes1@gmail.com>
2024-11-12 11:01:41 +0530

    this is minor changes

--- src/pages/todoList.jsx
+++ src/pages/todoList.jsx
@@ -25,6 +25,7 @@ const TodoList = () => {
   const [open, setOpen] = useState(false);
   const [detailsOpen, setDetailsOpen] = useState(false);
   const [selectedTaskId, setSelectedTaskId] = useState(null);
+  // const [columns, setColumns] = useState(["todo", "inprogress", "completed"]);
   const [editTask, setEditTask] = useState(null);
   const [newColumnName, setNewColumnName] = useState("");
   const dispatch = useDispatch();
